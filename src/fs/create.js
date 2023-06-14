29041c62c8d95e66ff25e03bcfeaf0181718efed
AlmondChips <chips6t@gmail.com>
2023-06-14 22:59:19 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs, { constants }  from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const files = __dirname + '/files'
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here 
+  const freshFilePath = files + '/Fresh.txt';
+  fs.open(freshFilePath, 'wx')
+  .then(() => {
+    fs.writeFile(freshFilePath, 'I am fresh and young')
+  })
+  .catch((err) => {throw new Error("FS operation failed")})
+
 };
 
 await create();
