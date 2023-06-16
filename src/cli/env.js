b906a6ce118cd93042576a3d85783c46ba372a1e
AlmondChips <chips6t@gmail.com>
2023-06-16 23:34:33 +0300

    feat: implement env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
     // Write your code here 
+    let result = '';
+
+    const envVars = Object.keys(process.env);
+    const RSSVars = envVars.filter((key) => key.slice(0, 4) === 'RSS_');
+    RSSVars.forEach((key) => result += `${key}=${process.env[key]}; `);
+    console.log(result);
 };
 
 parseEnv();
