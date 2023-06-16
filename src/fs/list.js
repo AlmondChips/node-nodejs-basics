da8589346b2a2860c2ef8d9119a9fced6d097b66
AlmondChips <chips6t@gmail.com>
2023-06-16 23:07:48 +0300

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import ERROR from '../consts/Error.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const files = __dirname + '/files';
+
 const list = async () => {
     // Write your code here 
+    fs.stat(files).then(async (data) => {
+      const filesList = await fs.readdir(files);
+      console.log('Files in folder: ', filesList);
+    }).catch(() => {throw new Error(ERROR)})
 };
 
 await list();
