bca2b77c5140c52ebec1332997c3ac8d7fd2985a
AlmondChips <chips6t@gmail.com>
2023-06-16 22:05:48 +0300

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import ERROR from '../consts/Error.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const files = __dirname + '/files';
+
 const remove = async () => {
     // Write your code here 
+    fs.unlink(files + '/fileToRemove.txt')
+    .catch(() => {throw new Error(ERROR)})
 };
 
 await remove();
