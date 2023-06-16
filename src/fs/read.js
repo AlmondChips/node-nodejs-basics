82c7a06cb090e93e9161125a074c6f207a6110d8
AlmondChips <chips6t@gmail.com>
2023-06-16 23:13:15 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import ERROR from '../consts/Error.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const files = __dirname + '/files';
+
 const read = async () => {
     // Write your code here 
+    fs.readFile(files + '/fileToRead.txt', { encoding: 'utf-8'})
+    .then((data) => console.log(`Content from file:\n${data}`, ))
+    .catch(() => {throw new Error(ERROR)});
 };
 
 await read();
