0f562e841d0b7f923d11fa17adb64efca1b6f334
AlmondChips <chips6t@gmail.com>
2023-06-17 16:29:56 +0300

    feat: implement args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
     // Write your code here 
+    const givenArgs = process.argv.splice(2);
+    let result = '';
+    for (let i = 0; i < givenArgs.length; i++) {
+      const element = givenArgs[i];
+      if (element.substring(0, 2) === '--') {
+        result += `${givenArgs[i].substring(2)} is ${givenArgs[i + 1]}, `
+        i++;
+      }
+    }
+    console.log(result.substring(0, result.length - 2));
 };
 
 parseArgs();
