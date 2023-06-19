f720f0c87b727e08b3a33d0e985c564f2af5bda6
AlmondChips <chips6t@gmail.com>
2023-06-20 02:19:33 +0300

    feat: implement worker-threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker } from 'worker_threads'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import os from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const CpuCores = os.cpus();
 const performCalculations = async () => {
     // Write your code here
+    const workerPromises = CpuCores.map((value, idx) => {
+      return new Promise((resolve, rejects) =>{ 
+        const worker = new Worker(__dirname + '/worker.js',{
+          workerData: {
+            value: 10 + idx,
+            path: '/worker.js',
+          }
+        })
+        worker.on('message', (data) => resolve({status: 'resolved', data}))
+        worker.on('error', () => rejects({status: 'error', data: null}))
+      })
+    })
+    
+    Promise.all(workerPromises)
+    .then((values) => console.log(values));
 };
 
 await performCalculations();
