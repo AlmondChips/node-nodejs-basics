10fe2df44f4b92a1817b78fe9a1f9c94ecbaa7b7
AlmondChips <chips6t@gmail.com>
2023-06-19 22:56:06 +0300

    feat: implement streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { stdout } from 'node:process';
+import fs from 'fs';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const files = __dirname + '/files';
+
 const read = async () => {
     // Write your code here 
+    const stream = fs.createReadStream(files + '/fileToRead.txt');
+    stream.on('data', (data) => {
+      stdout.write(data + '\n\n');
+    })
+
+
 };
 
 await read();
