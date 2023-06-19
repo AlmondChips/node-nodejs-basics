de4269dc4d2d620cb7b2e063280207bcfc9b211a
AlmondChips <chips6t@gmail.com>
2023-06-20 00:29:19 +0300

    fix: add fileToCompress removal after compressing

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,10 +1,8 @@
-import { stdin, stdout } from 'node:process';
 import { createGzip } from 'node:zlib';
 
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
-import { write } from 'node:fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,7 +15,11 @@ const compress = async () => {
     const readStream = fs.createReadStream(toCompress);
     const writeStream = fs.createWriteStream(compressTo);
 
-    writeStream.on('close', () => console.log('File has been succsessfully compressed!'));
+    writeStream.on('close', () => {
+      fs.unlink(toCompress,
+         () => console.log('File has been succsessfully compressed!'));
+    });
+
     readStream.pipe(createGzip()).pipe(writeStream);
 };
 
